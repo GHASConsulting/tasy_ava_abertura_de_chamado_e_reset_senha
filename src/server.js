00c7676b559ceab57eb2cf755c03406489cf688a
Fernando Lemos <fernando_lemos@hotmail.com.br>
2025-11-28 15:43:02 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ const connectionTasy = require("./connection")
 const runMigrations = require("./database/knex")
 const UserRepository = require("./repositories/UserCreateService")
 const routes = require("./routes")
-const getOracleData = require("./service/oracle")
 require("dotenv/config")
 const userRepository = new UserRepository()
 
@@ -12,23 +11,17 @@ express = require("express")
 
 const app = express()
 const port = 5000
-
-
 connectionTasy()
+runMigrations()
 
 
-getOracleData()
+app.use(express.json())
+app.use(routes)
 
-// runMigrations()
-
-
-// app.use(express.json())
-// app.use(routes)
-
-// userRepository.create({
-//     name: process.env.USER_SERVICE,
-//     password: process.env.PASSORWD
-// })
+userRepository.create({
+    name: process.env.USER_SERVICE,
+    password: process.env.PASSORWD
+})
 
 
 app.listen(port, () => console.log(`server is running on PORT ${port}`))
